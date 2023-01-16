c66772be1303e3de4dfae3e7279fbd5e464f8d0a
Bruno Castro <bruno.scastro2012@hotmail.com>
2023-01-15 22:20:34 -0300

    Subindo versao vscode

---
+++ src/ESP32AccessControl.ino
@@ -0,0 +1,131 @@
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSocketsServer.h>
+#include <ArduinoJson.h>
+#include <SPIFFS.h>
+
+const char *accessPointSSID = "Sala1307";
+const char *accessPointPassword = "Senhasala1307";
+
+IPAddress local_IP(192, 168, 13, 07);
+IPAddress gateway(192, 168, 1, 5);
+IPAddress subnet(255, 255, 255, 0);
+
+AsyncWebServer webServer(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(500);
+
+  initSPIFFS();
+  initAccessPoint();
+  initWebServer();
+  delay(500);
+  initWebSocket();
+};
+
+void loop()
+{
+  webSocket.loop();
+};
+
+void initSPIFFS()
+{
+  if (!SPIFFS.begin())
+  {
+    Serial.println("Cannot mount SPIFFS volume... Restarting in 5 seconds!");
+    delay(5000);
+    ESP.restart();
+  }
+};
+
+void initAccessPoint()
+{
+  Serial.print("Setting up Access Point ... ");
+  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+
+  Serial.print("Starting Access Point ... ");
+  Serial.println(WiFi.softAP(accessPointSSID, accessPointPassword) ? "Ready" : "Failed!");
+
+  Serial.print("IP address = ");
+  Serial.println(WiFi.softAPIP());
+};
+
+void initWebServer()
+{
+  webServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request) { // define here wat the webserver needs to do
+    request->send(SPIFFS, "/webpage.html", "text/html");
+  });
+
+  webServer.onNotFound([](AsyncWebServerRequest *request)
+                       { request->send(404, "text/plain", "Not found. Just '/' endpoint exists!"); });
+
+  webServer.serveStatic("/", SPIFFS, "/");
+};
+
+void initWebSocket()
+{
+  webSocket.begin();
+  webSocket.onEvent(onWebSocketEvent);
+
+  webServer.begin();
+};
+
+void sendCurrentStatus() {
+
+  Serial.println("Send Current Status to WEB -> {\"type\":\"getData\",\"isDoorOpen\":false,\"isRegistrationMode\":false\"}");
+
+  webSocket.broadcastTXT("{\"type\":\"getData\",\"isDoorOpen\":false,\"isRegistrationMode\":false}");
+};
+
+void handleValidEvents(String event_type, int event_value)
+{
+  if (event_type == "isRegistrationMode")
+  {
+    Serial.print("Set Registration Mode");
+    Serial.println(event_value == 1);
+    return;
+  }
+
+  if (event_type == "isDoorOpen")
+  {
+    Serial.print("Set Door Status");
+    Serial.println(event_value == 1);
+    return;
+  }
+
+  sendCurrentStatus();
+};
+
+void onWebSocketEvent(byte clientId, WStype_t eventType, uint8_t *payload, size_t length)
+{
+  switch (eventType)
+  {
+  case WStype_DISCONNECTED:
+    Serial.println("Client with ID" + String(clientId) + " disconnected now!");
+    break;
+  case WStype_CONNECTED:
+    Serial.println("Client with ID " + String(clientId) + " connected now!");
+    break;
+  case WStype_TEXT:
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, payload);
+
+    if (error)
+    {
+      Serial.print(F("Error on deserialize Json content: "));
+      Serial.println(error.f_str());
+      return;
+    }
+
+    const char *event_type = doc["eventType"];
+    const int event_value = doc["value"];
+
+    Serial.println("Type: " + String(event_type));
+    Serial.println("Value: " + String(event_value));
+
+    handleValidEvents(String(event_type), event_value);
+  };
+};
